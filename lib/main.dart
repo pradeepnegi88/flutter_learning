3d90d6937e46448abd09e07ecdedc02d5eea507f
PradeepNegi <pradeepnegi2012@gmail.com>
2020-08-19 20:42:14 +0530

    Refactored the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/widgets/listview.dart';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        canvasColor: Colors.green.shade100,
-      ),
-      title: 'Welcome to flutter app',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to flutter app"),
-        ),
-        body: RandomWords(),
-      ),
-    ));
+void main() => runApp(FavoriteList());
--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -1,5 +1,40 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
+class FavoriteList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    void _pushSave() {
+      // Navigator.of(context).push(
+      //   MaterialPageRoute(builder: (BuildContext context){
+      //      final tiles  = _saved
+      //   },)
+      // );
+    }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        canvasColor: Colors.green.shade100,
+      ),
+      title: 'Welcome to flutter app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to flutter app"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushSave,
+            ),
+          ],
+        ),
+        body: RandomWords(),
+      ),
+    );
+  }
+}
 
 class RandomWords extends StatefulWidget {
   @override
