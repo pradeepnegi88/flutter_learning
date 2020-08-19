51b1f9b7203129879dd1741acb741b73dc597040
PradeepNegi <pradeepnegi2012@gmail.com>
2020-08-19 16:54:31 +0530

    basic material design on top level

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/widgets/app.dart';
+import 'package:myapp/widgets/listview.dart';
 
-void main() => runApp(App());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        canvasColor: Colors.green.shade100,
+      ),
+      title: 'Welcome to flutter app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Welcome to flutter app"),
+        ),
+        body: RandomWords(),
+      ),
+    ));
