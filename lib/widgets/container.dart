94d76e3e27634d75abfaedc84133d838d945b3d2
PradeepNegi <pradeepnegi2012@gmail.com>
2020-08-25 16:38:30 +0530

    Added image in boxdecoration with border

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -14,7 +14,27 @@ class ContainerWidget extends StatelessWidget {
         body: Container(
           width: double.infinity,
           height: double.infinity,
-          child: Text("Container"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Container"),
+              Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          "https://flutter.dev/assets/flutter-lockup-1caf6476beed76adec3c477586da54de6b552b2f42108ec5bc68dc63bae2df75.png"),
+                    ),
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 5,
+                    )),
+              ),
+            ],
+          ),
           decoration: BoxDecoration(
             color: Colors.greenAccent,
           ),
