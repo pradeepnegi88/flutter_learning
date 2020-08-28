f06084feacbdd797af077059a01d1fd1a2d88aa6
PradeepNegi <pradeepnegi2012@gmail.com>
2020-08-28 18:26:19 +0530

    added custom dialog

--- lib/widgets/dialog.dart
+++ lib/widgets/dialog.dart
@@ -17,38 +17,104 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Dialog Examples"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            RaisedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    title: Text("My Alert Dialog"),
-                    content: Container(
-                      height: 300,
-                      child: Column(
-                        children: [
-                          Image.asset("images/lake.jpg", fit: BoxFit.cover),
-                          Text("This is the Alert Dialog content ........ "),
-                        ],
-                      ),
-                    ),
-                    actions: [
-                      RaisedButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text("Close"))
-                    ],
+      body: SimpleAlertDialogWidget(),
+    );
+  }
+}
+
+class SimpleAlertDialogWidget extends StatelessWidget {
+  const SimpleAlertDialogWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          RaisedButton(
+            onPressed: () {
+              showSimpleAlertDialog(context);
+            },
+            child: Text("Show Alert Dialog"),
+          ),
+          RaisedButton(
+            onPressed: () {
+              buildShowCustomDialog(context);
+            },
+            child: Text("Show Custom Dialog One "),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text("Show Custom Dialog Two "),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future buildShowCustomDialog(BuildContext context) {
+    return showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: Text(
+                    "Title",
+                    style: Theme.of(context).textTheme.headline4,
                   ),
-                );
-              },
-              child: Text("Show Dialog"),
-            )
-          ],
+                ),
+                Divider(color: Colors.grey),
+                TextField(
+                  decoration: InputDecoration(hintText: "Enter you data"),
+                ),
+                SizedBox(height: 20),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Save"),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future showSimpleAlertDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text("My Alert Dialog"),
+        content: Container(
+          height: 250,
+          child: Column(
+            children: [
+              Image.asset("images/lake.jpg", fit: BoxFit.cover),
+              Text("This is the Alert Dialog content ........ "),
+            ],
+          ),
         ),
+        actions: [
+          RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"))
+        ],
       ),
     );
   }
