8b663bf9074f418540fa72a3d28ef21e09147588
PradeepNegi <pradeepnegi2012@gmail.com>
2020-08-30 12:38:03 +0530

    Added Product list class

---
+++ lib/routerapp/routes/product_home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/routerapp/model/product.dart';
+
+class ProductHome extends StatelessWidget {
+  static const route = "/product";
+  @override
+  Widget build(BuildContext context) {
+    List<Product> products = List.generate(100,
+        (index) => Product(name: "Product $index", price: (index + 1) * 25));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product List"),
+      ),
+      body: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text("Product name : ${products[index].name}"),
+              trailing: Text("Price: ${products[index].price}"),
+            );
+          }),
+    );
+  }
+}
