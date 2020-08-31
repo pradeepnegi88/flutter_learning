a5cc81b5cb1b400aa75b9ef39a352f85fb7703cb
PradeepNegi <pradeepnegi2012@gmail.com>
2020-08-31 15:35:31 +0530

    Added radio

--- lib/switch.dart
+++ lib/switch.dart
@@ -7,9 +7,14 @@ class SwitchWidget extends StatefulWidget {
 
 class _SwitchWidgetState extends State<SwitchWidget> {
   bool toggle = false;
-
+  Color selectedColor;
   @override
   Widget build(BuildContext context) {
+    var onChanged2 = (newValue) {
+      setState(() {
+        selectedColor = newValue;
+      });
+    };
     return MaterialApp(
       title: "Switch Case",
       home: Scaffold(
@@ -17,24 +22,44 @@ class _SwitchWidgetState extends State<SwitchWidget> {
         appBar: AppBar(
           title: Text("Switch"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Dark Background",
-                style: TextStyle(fontSize: 30),
-              ),
-              Switch(
-                value: toggle,
-                onChanged: (newValue) {
-                  setState(() {
-                    toggle = newValue;
-                  });
-                },
-              )
-            ],
-          ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Dark Background",
+              style: TextStyle(fontSize: 30),
+            ),
+            Switch(
+              value: toggle,
+              onChanged: (newValue) {
+                setState(() {
+                  toggle = newValue;
+                });
+              },
+            ),
+            Row(
+              children: [
+                Text("Green"),
+                Radio(
+                  value: Colors.green,
+                  groupValue: selectedColor,
+                  activeColor: Colors.green,
+                  onChanged: onChanged2,
+                ),
+                Text("Red"),
+                Radio(
+                  value: Colors.red,
+                  groupValue: selectedColor,
+                  activeColor: Colors.red,
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedColor = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
