2c23f83049916038c34bba91870b497bd8b1e03f
PradeepNegi <pradeepnegi2012@gmail.com>
2020-09-02 17:04:45 +0530

    Added search filter

--- lib/sqlite/sqlite_home.dart
+++ lib/sqlite/sqlite_home.dart
@@ -12,10 +12,40 @@ class SqliteHome extends StatefulWidget {
 
 class _SqliteHomeState extends State<SqliteHome> {
   DBHelper dbHelper;
+  List allCourses = List();
+  List items = List();
+  TextEditingController textSearch = TextEditingController();
   @override
   void initState() {
     super.initState();
     dbHelper = DBHelper();
+    dbHelper.allCourses().then((value) {
+      setState(() {
+        allCourses = value;
+        items = allCourses;
+      });
+    });
+  }
+
+  void filterSearch(String query) {
+    var dummySearchList = List();
+    if (query.isNotEmpty) {
+      allCourses.forEach((element) {
+        var course = Course.fromMap(element);
+        if (course.name.toLowerCase().contains(query.toLowerCase())) {
+          dummySearchList.add(element);
+        }
+        setState(() {
+          items = [];
+          items.addAll(dummySearchList);
+        });
+      });
+    } else {
+      setState(() {
+        items = [];
+        items = allCourses;
+      });
+    }
   }
 
   @override
@@ -33,39 +63,61 @@ class _SqliteHomeState extends State<SqliteHome> {
           )
         ],
       ),
-      body: FutureBuilder(
-          future: dbHelper.allCourses(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
-            } else {
-              return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    Course course = Course.fromMap(snapshot.data[index]);
-                    return Card(
-                      child: ListTile(
-                        title: Text('${course.name} - ${course.hours} hours'),
-                        subtitle: Text(course.content),
-                        trailing: IconButton(
-                          icon: Icon(
-                            Icons.delete,
-                            color: Colors.red,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  filterSearch(value);
+                });
+              },
+              controller: textSearch,
+              decoration: InputDecoration(
+                labelText: "Search",
+                hintText: "Search ....",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      Course course = Course.fromMap(items[index]);
+                      return Card(
+                        child: ListTile(
+                          title: Text('${course.name} - ${course.hours} hours'),
+                          subtitle: Text(course.content),
+                          trailing: IconButton(
+                            icon: Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            ),
+                            onPressed: () async {
+                              await dbHelper.deleteCourse(course.id);
+                            },
                           ),
-                          onPressed: () async {
-                            await dbHelper.deleteCourse(course.id);
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) =>
+                                    ShowCourse(course: course)));
                           },
                         ),
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) =>
-                                  ShowCourse(course: course)));
-                        },
-                      ),
-                    );
-                  });
-            }
-          }),
+                      );
+                    }),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
