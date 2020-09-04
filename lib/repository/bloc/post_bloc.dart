34958e4dcbafd057ab41f2f48ca3b2ed377ab883
PradeepNegi <pradeepnegi2012@gmail.com>
2020-09-04 13:35:19 +0530

    Added bloc

--- lib/repository/bloc/post_bloc.dart
+++ lib/repository/bloc/post_bloc.dart
@@ -2,17 +2,29 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:myapp/repository/data/repo/post_repository.dart';
+import 'package:myapp/repository/model/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial());
+  final PostRepository postRepository;
+
+  PostBloc(this.postRepository) : super(PostInitial());
 
   @override
   Stream<PostState> mapEventToState(
     PostEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is PostEvent) {
+      yield PostLoading();
+      try {
+        final posts = await postRepository.getPost();
+        yield PostLoaded(posts);
+      } catch (e) {
+        yield PostError(e.toString());
+      }
+    }
   }
 }
