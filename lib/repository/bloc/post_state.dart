58862b20a919e59e6af9ae7ad5f80981c6795859
PradeepNegi <pradeepnegi2012@gmail.com>
2020-09-04 13:34:44 +0530

    State added

--- lib/repository/bloc/post_state.dart
+++ lib/repository/bloc/post_state.dart
@@ -4,3 +4,15 @@ part of 'post_bloc.dart';
 abstract class PostState {}
 
 class PostInitial extends PostState {}
+
+class PostLoading extends PostState {}
+
+class PostLoaded extends PostState {
+  List<Post> posts;
+  PostLoaded(this.posts);
+}
+
+class PostError extends PostState {
+  String message;
+  PostError(this.message);
+}
