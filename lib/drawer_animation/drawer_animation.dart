5095cbb549853331c618c6d642080e9f76eb40c2
PradeepNegi <pradeepnegi2012@gmail.com>
2020-09-12 12:53:43 +0530

    TweenAnimation

--- lib/drawer_animation/drawer_animation.dart
+++ lib/drawer_animation/drawer_animation.dart
@@ -29,31 +29,60 @@ class _DrawerMainAnimationWidgetState extends State<DrawerMainAnimationWidget> {
         appBar: AppBar(
           title: Text("Animation"),
         ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
-              onPressed: () {
-                _updateState();
-              },
-              child: Text("Animate"),
+        body: TweenAnimationWidget(),
+      ),
+    );
+  }
+
+  Widget animatedContainerWidget(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RaisedButton(
+          onPressed: () {
+            _updateState();
+          },
+          child: Text("Animate"),
+        ),
+        AnimatedContainer(
+          curve: Curves.easeInOut,
+          duration: Duration(milliseconds: 600),
+          width: _width,
+          height: _height,
+          color: Colors.red,
+          child: Center(
+            child: Text(
+              "Animation",
+              style: Theme.of(context).textTheme.headline5,
             ),
-            AnimatedContainer(
-              curve: Curves.easeInOut,
-              duration: Duration(milliseconds: 600),
-              width: _width,
-              height: _height,
-              color: Colors.red,
-              child: Center(
-                child: Text(
-                  "Animation",
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              ),
-            ),
-          ],
-        )),
+          ),
+        ),
+      ],
+    ));
+  }
+}
+
+class TweenAnimationWidget extends StatefulWidget {
+  @override
+  _TweenAnimationWidgetState createState() => _TweenAnimationWidgetState();
+}
+
+class _TweenAnimationWidgetState extends State<TweenAnimationWidget> {
+  Tween<double> _scaleDown = Tween<double>(begin: 1, end: 2);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TweenAnimationBuilder(
+        tween: _scaleDown,
+        builder: (context, scale, child) {
+          return Transform.scale(scale: scale, child: child);
+        },
+        duration: Duration(milliseconds: 600),
+        child: Container(
+            color: Colors.teal,
+            child: Text("Animation",
+                style: Theme.of(context).textTheme.headline5)),
       ),
     );
   }
